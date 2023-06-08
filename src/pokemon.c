65c7db0f372a3f335c4cf6bc03d34e71042518c2
Yashi Mizu <yashi97250.91@gmail.com>
2023-06-08 12:10:38 +0200

    MidBattleEvo

--- src/pokemon.c
+++ src/pokemon.c
@@ -5671,7 +5671,7 @@ void PokemonToBattleMon(struct Pokemon *src, struct BattlePokemon *dst)
     dst->status2 = 0;
 }
 
-void CopyPlayerPartyMonToBattleData(u8 battlerId, u8 partyIndex)
+void CopyPlayerPartyMonToBattleData(u8 battlerId, u8 partyIndex, bool8 resetStats)
 {
     PokemonToBattleMon(&gPlayerParty[partyIndex], &gBattleMons[battlerId]);
     gBattleStruct->hpOnSwitchout[GetBattlerSide(battlerId)] = gBattleMons[battlerId].hp;
@@ -5915,6 +5915,16 @@ bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mov
                             itemEffectParam++;
                             break;
                         }
+                            if (gMain.inBattle)
+                            {
+                                if (battlerId != MAX_BATTLERS_COUNT)
+                                {
+                                    gAbsentBattlerFlags &= ~gBitTable[battlerId];
+                                    CopyPlayerPartyMonToBattleData(battlerId, GetPartyIdFromBattlePartyId(gBattlerPartyIndexes[battlerId]), TRUE);
+                                    if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
+                                        gBattleResults.numRevivesUsed++;
+                                }
+                            }
 
                         // Get amount of HP to restore
                         dataUnsigned = itemEffect[itemEffectParam++];
